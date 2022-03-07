4bcb3a3d56cb6e417deda2bdf75be4483573ebed
Raphael Reme <raphael.reme@telecom-paristech.fr>
2022-03-07 11:25:31 +0100

    Build a deb package with the executable for linux

---
+++ deploy/linux/build.sh
@@ -0,0 +1,19 @@
+set -e
+
+pyinstaller BOOM.spec
+
+PACKAGE_NAME=boom_$(boom_version)-1_all
+
+mkdir -p $PACKAGE_NAME/usr/local/bin
+mkdir -p $PACKAGE_NAME/usr/share/applications
+mkdir -p $PACKAGE_NAME/DEBIAN
+
+cp dist/BOOM $PACKAGE_NAME/usr/local/bin
+cp -r boom/usr/share/BOOM $PACKAGE_NAME/usr/share
+
+sed "s/<version>/$(boom_version)/g" boom/DEBIAN/control.template >> $PACKAGE_NAME/DEBIAN/control
+sed "s/<version>/$(boom_version)/g" boom/usr/share/applications/BOOM.desktop.template >> $PACKAGE_NAME/usr/share/applications/BOOM.desktop
+
+dpkg-deb --build --root-owner-group $PACKAGE_NAME
+
+

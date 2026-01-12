303b5d153feeb24febcfc71d2a6acb3a24391a9a
Raphael Reme <raphaelreme@protonmail.com>
2026-01-12 11:32:21 +0100

    Minor fixes for CI to run

--- deploy/linux/build.sh
+++ deploy/linux/build.sh
@@ -2,7 +2,9 @@ set -e
 
 uv run pyinstaller BOOM.spec
 
-PACKAGE_NAME=boom_$(boom_version)-1_all
+BOOM_VERSION=$(uv run boom_version)
+
+PACKAGE_NAME=boom_$BOOM_VERSION-1_all
 
 mkdir -p $PACKAGE_NAME/usr/local/bin
 mkdir -p $PACKAGE_NAME/usr/share/applications
@@ -11,8 +13,8 @@ mkdir -p $PACKAGE_NAME/DEBIAN
 cp dist/BOOM $PACKAGE_NAME/usr/local/bin
 cp -r boom/usr/share/BOOM $PACKAGE_NAME/usr/share
 
-sed "s/<version>/$(boom_version)/g" boom/DEBIAN/control.template >> $PACKAGE_NAME/DEBIAN/control
-sed "s/<version>/$(boom_version)/g" boom/usr/share/applications/BOOM.desktop.template >> $PACKAGE_NAME/usr/share/applications/BOOM.desktop
+sed "s/<version>/$BOOM_VERSION/g" boom/DEBIAN/control.template >> $PACKAGE_NAME/DEBIAN/control
+sed "s/<version>/$BOOM_VERSION/g" boom/usr/share/applications/BOOM.desktop.template >> $PACKAGE_NAME/usr/share/applications/BOOM.desktop
 
 dpkg-deb --build --root-owner-group $PACKAGE_NAME
 
